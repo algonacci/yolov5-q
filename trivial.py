4358e2203db2f0e9fa4d8be54704a52cc277a760
Laughing-q <1185102784@qq.com>
2021-11-30 16:40:49 +0800

    update plot_results

---
+++ trivial.py
@@ -0,0 +1,3 @@
+from utils.plots import plot_results
+
+plot_results(file='runs/no_aug/sleep/results.csv')
--- utils/plots.py
+++ utils/plots.py
@@ -394,7 +394,7 @@ def plot_evolve(evolve_csv='path/to/evolve.csv'):  # from utils.plots import *; 
     print(f'Saved {f}')
 
 
-def plot_results(file='path/to/results.csv', dir=''):
+def plot_results(file='path/to/results.csv', dir='', best=True):
     # Plot training results.csv. Usage: from utils.plots import *; plot_results('path/to/results.csv')
     save_dir = Path(file).parent if file else Path(dir)
     fig, ax = plt.subplots(2, 5, figsize=(12, 6), tight_layout=True)
@@ -411,10 +411,16 @@ def plot_results(file='path/to/results.csv', dir=''):
                 y = data.values[:, j]
                 # y[y == 0] = np.nan  # don't show zero values
                 ax[i].plot(x, y, marker='.', label=f.stem, linewidth=2, markersize=2)
-                ax[i].set_title(s[j], fontsize=12)
-                ax[i].scatter(index, y[index], color='r',
-                              label='best', marker='*', linewidth=3)
-                ax[i].set_title(s[i] + f'\n{round(y[index], 5)}')
+                if best:
+                    # best
+                    ax[i].scatter(index, y[index], color='r',
+                                  label='best', marker='*', linewidth=3)
+                    ax[i].set_title(s[j] + f'\n{round(y[index], 5)}')
+                else:
+                    # last
+                    ax[i].scatter(x[-1], y[-1], color='r',
+                                  label='best', marker='*', linewidth=3)
+                    ax[i].set_title(s[j] + f'\n{round(y[-1], 5)}')
                 # if j in [8, 9, 10]:  # share train and val loss y axes
                 #     ax[i].get_shared_y_axes().join(ax[i], ax[i - 5])
         except Exception as e:
