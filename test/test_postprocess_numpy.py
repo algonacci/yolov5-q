99756cdb0177f3f7485e1e0e21db181e44230d11
Laughing-q <1185102784@qq.com>
2022-04-13 14:37:59 +0800

    add nms_numpy

--- test/test_postprocess_numpy.py
+++ test/test_postprocess_numpy.py
@@ -7,32 +7,34 @@ input:
 """
 
 import numpy as np
-import torch
 import cv2
-from yolov5.utils.boxes import non_max_suppression, scale_coords
+from yolov5.utils.boxes import non_max_suppression_numpy, scale_coords
 from yolov5.utils.plots import Visualizer
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 class TestPost:
     def __init__(self, preds=[]) -> None:
         self.stride = [8, 16, 32]
-        self.preds = [torch.from_numpy(np.load(p)) for p in preds]
+        self.preds = [np.load(p) for p in preds]
         self.nl = len(self.preds)
         self.na = self.preds[0].shape[1]
         self.no = self.preds[0].shape[-1]
 
-        self.grid = [torch.zeros(1)] * self.nl  # init grid
-        self.anchor_grid = [torch.zeros(1)] * self.nl  # init anchor grid
+        self.grid = [np.zeros(1)] * self.nl  # init grid
+        self.anchor_grid = [np.zeros(1)] * self.nl  # init anchor grid
         self.anchors = (
-            torch.tensor(
+            np.array(
                 [
                     [10, 13, 16, 30, 33, 23],
                     [30, 61, 62, 45, 59, 119],
                     [116, 90, 156, 198, 373, 326],
                 ]
             )
-            .float()
-            .view(self.nl, -1, 2)
+            .astype(np.float32)
+            .reshape(self.nl, -1, 2)
         )
         self.vis = Visualizer(names=list(range(self.no - 5)))
 
@@ -42,23 +44,23 @@ class TestPost:
             _, _, ny, nx, _ = self.preds[i].shape  # (bs, 3, 20, 20, 85)
             self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
             print(self.grid[i].shape, self.anchor_grid[i].shape)
-            y = self.preds[i].sigmoid()
+            y = sigmoid(self.preds[i])
             xy = (y[..., 0:2] * 2.0 - 0.5 + self.grid[i]) * self.stride[i]  # xy
             wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-            y = torch.cat((xy.type_as(y), wh.type_as(y), y[..., 4:]), -1)
-            z.append(y.view(-1, self.na * ny * nx, self.no))
-        output = torch.cat(z, 1)
+            y = np.concatenate((xy, wh, y[..., 4:]), -1)
+            z.append(y.reshape(-1, self.na * ny * nx, self.no))
+        output = np.concatenate(z, 1)
         return output
 
     def _make_grid(self, nx=20, ny=20, i=0):
-        d = self.anchors[i].device
-        yv, xv = torch.meshgrid([torch.arange(ny).to(d), torch.arange(nx).to(d)])
-        grid = torch.stack((xv, yv), 2).expand((1, self.na, ny, nx, 2)).float()
+        yv, xv = np.meshgrid(np.arange(ny), np.arange(nx))
+        grid = np.stack((xv.T, yv.T), 2)[None, None, ...].repeat(3, axis=1).astype(np.float32)
         anchor_grid = (
-            (self.anchors[i].clone())
-            .view((1, self.na, 1, 1, 2))
-            .expand((1, self.na, ny, nx, 2))
-            .float()
+            (self.anchors[i].copy())
+            .reshape((1, self.na, 1, 1, 2))
+            .repeat(ny, 2)
+            .repeat(nx, 3)
+            .astype(np.float32)
         )
         return grid, anchor_grid
 
@@ -77,7 +79,7 @@ if __name__ == "__main__":
     test = TestPost(preds=preds)
     output = test()
     print(output.shape)
-    output = non_max_suppression(output, conf_thres=0.1)
+    output = non_max_suppression_numpy(output, conf_thres=0.2)
     print(output)
 
     name = 'smoke_phone.jpg'
@@ -89,7 +91,7 @@ if __name__ == "__main__":
         det[:, :4] = scale_coords((416, 768), det[:, :4], img.shape[:2]).round()
 
     print(output)
-    img = test.vis(img, output, vis_confs=0.0)
-    # cv2.imwrite(f'int8/{name}', img)
+    # img = test.vis(img, output, vis_confs=0.0)
+    # # cv2.imwrite(f'int8/{name}', img)
     # cv2.imshow('p', img)
     # cv2.waitKey(0)
--- yolov5/utils/boxes.py
+++ yolov5/utils/boxes.py
@@ -4,6 +4,7 @@ import time
 import torchvision
 import cv2
 
+from lqcv.bbox.utils import nms_numpy
 from .general import increment_path
 from .metrics import box_iou
 
@@ -207,6 +208,96 @@ def non_max_suppression(
                 i = i[iou.sum(1) > 1]  # require redundancy
 
         output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f"WARNING: NMS time limit {time_limit}s exceeded")
+            break  # time limit exceeded
+
+    return output
+
+def non_max_suppression_numpy(
+    prediction,
+    conf_thres=0.25,
+    iou_thres=0.45,
+    classes=None,
+    agnostic=False,
+    multi_label=False,
+    labels=(),
+    max_det=300,
+):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    nc = prediction.shape[2] - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Checks
+    assert (
+        0 <= conf_thres <= 1
+    ), f"Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0"
+    assert (
+        0 <= iou_thres <= 1
+    ), f"Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0"
+
+    # Settings
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+
+    t = time.time()
+    output = [np.zeros((0, 6))] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            l = labels[xi]
+            v = np.zeros((len(l), nc + 5), device=x.device)
+            v[:, :4] = l[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
+            x = np.concatenate((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = np.concatenate((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1), x[:, 5:].argmax(1)
+            x = np.concatenate((box, conf[:, None], j.astype(np.float)[:, None]), 1)[conf > conf_thres]
+
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == np.array(classes)).any(1)]
+
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # Batched NMS
+        boxes, scores, cls = x[:, :4], x[:, 4], x[:, 5]
+        i = nms_numpy(boxes, scores, cls, iou_thres, agnostic)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+
+        output[xi] = x[i][None, :] if x[i].ndim == 1 else x[i]
         if (time.time() - t) > time_limit:
             print(f"WARNING: NMS time limit {time_limit}s exceeded")
             break  # time limit exceeded
