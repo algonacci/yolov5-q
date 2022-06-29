2667db4d72d5ed06154ec811fac7458005e07ede
Laughing-q <1182102784@qq.com>
2022-06-29 09:25:35 +0800

    update

--- tools/others/trivial.py
+++ tools/others/trivial.py
@@ -2,5 +2,6 @@ from yolov5.utils.plots import plot_results, plot_results_with_masks
 from yolov5.utils.general import strip_optimizer
 
 # plot_results(file='runs/no_aug/phone/results.csv')
-plot_results_with_masks(file='runs/train/balloon2/results.csv')
+plot_results(file='', dir='runs/train')
+# plot_results_with_masks(file='runs/train/balloon2/results.csv')
 # strip_optimizer('/home/laughing/yolov5/runs/1230/weights/best.pt')
